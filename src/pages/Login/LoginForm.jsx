f009431be242efdc6180f3c8fdae38056510e489
mominul <ulhouqemomin39@gmail.com>
2023-06-07 10:54:35 +0600

    added sweetalreat

--- src/pages/Login/LoginForm.jsx
+++ src/pages/Login/LoginForm.jsx
@@ -1,100 +1,109 @@
 import React, { useState, useContext } from 'react';
 import { useForm } from 'react-hook-form';
-
 import { FaEye, FaEyeSlash, FaGoogle } from 'react-icons/fa';
-import { Link , useNavigate, useLocation} from 'react-router-dom';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
 import { AuthContext } from '../../provider/AuthProvider';
+import Swal from 'sweetalert2';
 
 const LoginForm = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm();
-    const [passwordVisible, setPasswordVisible] = useState(false);
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const [passwordVisible, setPasswordVisible] = useState(false);
 
-    // const {singIn} = useContext(AuthContext);
-    // const navigate = useNavigate();
-    // const location = useLocation();
-    // const from = location.state?.from?.pathname || '/'
+  const { signIn } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || '/';
 
+  const onSubmit = (data) => {
+    // Handle login logic here
+    console.log(data);
+    signIn(data.email, data.password)
+      .then(result => {
+        const loggedUser = result.user;
+        console.log(loggedUser);
+        Swal.fire({
+          icon: 'success',
+          title: 'Login Successful',
+          text: 'You have successfully logged in!',
+        });
+      })
+      .catch(error => {
+        Swal.fire({
+          icon: 'error',
+          title: 'Login Failed',
+          text: error.message,
+        });
+        console.log(error.message);
+      });
+  };
 
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
+  };
 
-
-    const onSubmit = (data) => {
-        // Handle login logic here
-        console.log(data);
-// singIn(email,  password)
-// .then(result =>)
-
-
-
-    };
-
-    const togglePasswordVisibility = () => {
-        setPasswordVisible(!passwordVisible);
-    };
-
-    return (
-        <div className="flex  justify-center  items-center  bg-gray-100">
-            <div className="bg-white max-w-sm mx-auto p-6 rounded-lg my-12 shadow-md">
-                <h2 className="text-2xl font-semibold mb-4">Please Login !!!</h2>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <div className="mb-4">
-                        <label htmlFor="email" className="block text-gray-700 font-medium mb-1">
-                            Email
-                        </label>
-                        <input
-                            type="email"
-                            id="email"
-                            className="w-full border-gray-300 border py-2 px-3 rounded focus:outline-none focus:ring focus:ring-blue-200"
-                            {...register('email', { required: true })}
-                        />
-                        {errors.email && <span className="text-red-500">Email is required</span>}
-                    </div>
-                    <div className="mb-4 relative items-center">
-                        <label htmlFor="password" className="block text-gray-700 font-medium mb-1">
-                            Password
-                        </label>
-                        <div className='flex items-center'>
-                            <input
-                                type={passwordVisible ? 'text' : 'password'}
-                                id="password"
-                                className="w-full border-gray-300 border py-2 px-3 rounded focus:outline-none focus:ring focus:ring-blue-200"
-                                {...register('password', { required: true })}
-                            />
-                            <button
-                                type="button"
-                                className="absolute my-auto  right-3 "
-                                onClick={togglePasswordVisibility}
-                            >
-                                {passwordVisible ? (
-                                    <FaEyeSlash className="h-5 w-5 text-gray-600" />
-                                ) : (
-                                    <FaEye className="h-5 w-5 text-gray-600" />
-                                )}
-                            </button>
-                        </div>
-                        {errors.password && <span className="text-red-500">Password is required</span>}
-                    </div>
-                    <button
-                        type="submit"
-                        className="btn btn-primary btn-md py-2 px-4 rounded focus:outline-none focus:ring focus:ring-blue-200"
-                    >
-                        Login
-                    </button>
-                </form>
-                <div className="mt-4">
-                    <Link to="/register" className="text-blue-500">Create an account</Link>
-                </div>
-                <div className="mt-4 mx-auto">
-                    <button
-                        type="button"
-                        className="py-2 btn btn-info btn-outline px-4 rounded-full focus:outline-none focus:ring focus:ring-blue-200"
-                    >
-                        <FaGoogle /> sign with google
-
-                    </button>
-                </div>
+  return (
+    <div className="flex justify-center items-center bg-gray-100">
+      <div className="bg-white max-w-sm mx-auto p-6 rounded-lg my-12 shadow-md">
+        <h2 className="text-2xl font-semibold mb-4">Please Login !!!</h2>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="mb-4">
+            <label htmlFor="email" className="block text-gray-700 font-medium mb-1">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="w-full border-gray-300 border py-2 px-3 rounded focus:outline-none focus:ring focus:ring-blue-200"
+              {...register('email', { required: true })}
+            />
+            {errors.email && <span className="text-red-500">Email is required</span>}
+          </div>
+          <div className="mb-4 relative items-center">
+            <label htmlFor="password" className="block text-gray-700 font-medium mb-1">
+              Password
+            </label>
+            <div className="flex items-center">
+              <input
+                type={passwordVisible ? 'text' : 'password'}
+                id="password"
+                className="w-full border-gray-300 border py-2 px-3 rounded focus:outline-none focus:ring focus:ring-blue-200"
+                {...register('password', { required: true })}
+              />
+              <button
+                type="button"
+                className="absolute my-auto right-3"
+                onClick={togglePasswordVisibility}
+              >
+                {passwordVisible ? (
+                  <FaEyeSlash className="h-5 w-5 text-gray-600" />
+                ) : (
+                  <FaEye className="h-5 w-5 text-gray-600" />
+                )}
+              </button>
             </div>
+            {errors.password && <span className="text-red-500">Password is required</span>}
+          </div>
+          <button
+            type="submit"
+            className="btn btn-primary btn-md py-2 px-4 rounded focus:outline-none focus:ring focus:ring-blue-200"
+          >
+            Login
+          </button>
+        </form>
+        <div className="mt-4">
+          <Link to="/register" className="text-blue-500">Create an account</Link>
         </div>
-    );
+        <div className="mt-4 mx-auto">
+          <button
+            type="button"
+            className="py-2 btn btn-info btn-outline px-4 rounded-full focus:outline-none focus:ring focus:ring-blue-200"
+          >
+            <FaGoogle /> Sign in with Google
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LoginForm;
--- src/pages/RegisterForm/RegistrationForm.jsx
+++ src/pages/RegisterForm/RegistrationForm.jsx
@@ -3,136 +3,153 @@ import { useForm } from 'react-hook-form';
 import { FaGoogle } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../provider/AuthProvider';
+import { updateCurrentUser } from 'firebase/auth';
+import Swal from 'sweetalert2';
 
 const RegistrationForm = () => {
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-    const {createUser} = useContext(AuthContext)
+  const { createUser, updatedUserProfiles } = useContext(AuthContext);
 
-    const onSubmit = (data) => {
-        // Handle registration logic here
-        console.log(data);
-        createUser(data.email, data.password)
-        .then(result =>{
-            const createdUser = result.user
-            console.log(createdUser);
-        })
-        .catch(error =>console.log(error.message))
+  const onSubmit = (data) => {
+    // Handle registration logic here
+    console.log(data);
+    createUser(data.email, data.password)
+      .then((result) => {
+        const createdUser = result.user;
 
+        Swal.fire({
+          icon: 'success',
+          title: 'Registration Successful',
+          text: 'You have successfully registered.',
+        });
 
+        navigate('/login');
+        return updatedUserProfiles(auth.currentUser, {
+          displayName: data.name,
+          photoURL: data.photoURL,
+        });
+      })
+      .catch((error) => {
+        Swal.fire({
+          icon: 'error',
+          title: 'Registration Failed',
+          text: error.message,
+        });
+        console.log(error.message);
+      });
+  };
 
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="my-12 rounded-xl shadow-2xl p-6 max-w-sm mx-auto"
+    >
+      <h1 className="font-bold text-2xl my-6">Please Register Here !!!</h1>
+      <div className="mb-4">
+        <label htmlFor="name" className="block mb-2 font-medium">
+          Name
+        </label>
+        <input
+          type="text"
+          id="name"
+          {...register('name', { required: true })}
+          className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+      </div>
+      <div className="mb-4">
+        <label htmlFor="photoUrl" className="block mb-2 font-medium">
+          Photo URL
+        </label>
+        <input
+          type="text"
+          id="photoUrl"
+          {...register('photoUrl')}
+          className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+      </div>
+      <div className="mb-4">
+        <label htmlFor="email" className="block mb-2 font-medium">
+          Email
+        </label>
+        <input
+          type="email"
+          id="email"
+          {...register('email', { required: true })}
+          className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+      </div>
+      <div className="flex gap-3">
+        <div className="mb-4 w-1/2">
+          <label htmlFor="password" className="block mb-2 font-medium">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            {...register('password', {
+              required: true,
+              minLength: 6,
+              pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
+            })}
+            className={`w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 ${
+              errors.password ? 'focus:ring-red-500' : 'focus:ring-blue-500'
+            }`}
+          />
+          {errors.password && (
+            <p className="text-red-500 text-sm">
+              Password must be at least 6 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character.
+            </p>
+          )}
+        </div>
+        <div className="mb-4 w-1/2">
+          <label htmlFor="confirmPassword" className="block mb-2 font-medium">
+            Confirm Password
+          </label>
+          <input
+            type="password"
+            id="confirmPassword"
+            {...register('confirmPassword', {
+              required: true,
+              validate: (value) => value === document.getElementById('password').value,
+            })}
+            className={`w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 ${
+              errors.confirmPassword ? 'focus:ring-red-500' : 'focus:ring-blue-500'
+            }`}
+          />
+          {errors.confirmPassword && (
+            <p className="text-red-500 text-sm">Passwords do not match.</p>
+          )}
+        </div>
+      </div>
+      <div className="my-4">
+        <Link to="/login" className="text-blue-500">Already have an account </Link>
+      </div>
 
-    };
-
-    return (
-        <form onSubmit={handleSubmit(onSubmit)} className="my-12 rounded-xl shadow-2xl p-6 max-w-sm mx-auto">
-            <h1 className='font-bold text-2xl my-6'>Please Register Here !!!</h1>
-            <div className="mb-4">
-                <label htmlFor="name" className="block mb-2 font-medium">
-                    Name
-                </label>
-                <input
-                    type="text"
-                    id="name"
-                    {...register('name', { required: true })}
-                    className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                />
-            </div>
-            <div className="mb-4">
-                <label htmlFor="photoUrl" className="block mb-2 font-medium">
-                    Photo URL
-                </label>
-                <input
-                    type="text"
-                    id="photoUrl"
-                    {...register('photoUrl')}
-                    className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                />
-            </div>
-            <div className="mb-4">
-                <label htmlFor="email" className="block mb-2 font-medium">
-                    Email
-                </label>
-                <input
-                    type="email"
-                    id="email"
-                    {...register('email', { required: true })}
-                    className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                />
-            </div>
-            <div className='flex  gap-3'>
-                <div className="mb-4 w-1/2">
-                    <label htmlFor="password" className="block mb-2 font-medium">
-                        Password
-                    </label>
-                    <input
-                        type="password"
-                        id="password"
-                        {...register('password', {
-                            required: true,
-                            minLength: 6,
-                            pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
-                        })}
-                        className={`w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 ${errors.password ? 'focus:ring-red-500' : 'focus:ring-blue-500'
-                            }`}
-                    />
-                    {errors.password && (
-                        <p className="text-red-500 text-sm">
-                            Password must be at least 6 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character.
-                        </p>
-                    )}
-                </div>
-                <div className="mb-4 w-1/2">
-                    <label htmlFor="confirmPassword" className="block mb-2 font-medium">
-                        Confirm Password
-                    </label>
-                    <input
-                        type="password"
-                        id="confirmPassword"
-                        {...register('confirmPassword', {
-                            required: true,
-                            validate: (value) => value === document.getElementById('password').value,
-                        })}
-                        className={`w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 ${errors.confirmPassword ? 'focus:ring-red-500' : 'focus:ring-blue-500'
-                            }`}
-                    />
-                    {errors.confirmPassword && (
-                        <p className="text-red-500 text-sm">Passwords do not match.</p>
-                    )}
-                </div>
-            </div>
-            <div className="my-4">
-                    <Link to="/login" className="text-blue-500">Already have an account </Link>
-                </div>
-
-
-
-
-            {/* Social Login */}
-            <div className=' justify-between '>
-
-                <div className="mb-4">
-                    <input
-                        type="submit"
-                        value="Register"
-                        className="w-full px-4 py-2 btn btn-primary cursor-pointer"
-                    />
-                </div>
-                <div className="mb-4">
-                    <button
-                        className="w-full btn btn-outline btn-primary  px-4 py-2 cursor-pointer"
-                        onClick={() => console.log('Social login clicked')}
-                    >
-                        <FaGoogle /> Social Login
-                    </button>
-                </div>
-            </div>
-        </form>
-    );
+      {/* Social Login */}
+      <div className="justify-between">
+        <div className="mb-4">
+          <input
+            type="submit"
+            value="Register"
+            className="w-full px-4 py-2 btn btn-primary cursor-pointer"
+          />
+        </div>
+        <div className="mb-4">
+          <button
+            className="w-full btn btn-outline btn-primary  px-4 py-2 cursor-pointer"
+            onClick={() => console.log('Social login clicked')}
+          >
+            <FaGoogle /> Social Login
+          </button>
+        </div>
+      </div>
+    </form>
+  );
 };
 
 export default RegistrationForm;
