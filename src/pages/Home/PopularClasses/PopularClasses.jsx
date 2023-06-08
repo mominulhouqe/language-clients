ecfed113a1ed07816e5d94f7a48c4362c3cbc57e
mominul <ulhouqemomin39@gmail.com>
2023-06-08 11:27:00 +0600

    updated popular clasess

--- src/pages/Home/PopularClasses/PopularClasses.jsx
+++ src/pages/Home/PopularClasses/PopularClasses.jsx
@@ -1,38 +1,45 @@
 import React from "react";
 import { FaVideo } from "react-icons/fa";
+import useMenu from "../../../hooks/useMenu";
+import { log } from "react-modal/lib/helpers/ariaAppHider";
 
-const popularClassesData = [
-  {
-    id: 1,
-    image: "https://i.ibb.co/KwFsQVB/my-img1-1.jpg",
-    title: "Yoga Class",
-    students: 150,
-  },
-  {
-    id: 2,
-    image: "https://i.ibb.co/NyQhDBZ/my-img1.jpg",
-    title: "Dance Workshop",
-    students: 1520,
-  },
-  {
-    id: 3,
-    image: "https://i.ibb.co/37bdwrH/my-img1.png",
-    title: "Cooking Class",
-    students: 100,
-  },
-  {
-    id: 4,
-    image: "https://i.ibb.co/37bdwrH/my-img1.png",
-    title: "Cooking Class",
-    students: 7600,
-  },
-  // Add more class data
-];
+// const popularClassesData = [
+//   {
+//     id: 1,
+//     image: "https://i.ibb.co/KwFsQVB/my-img1-1.jpg",
+//     title: "Yoga Class",
+//     students: 150,
+//   },
+//   {
+//     id: 2,
+//     image: "https://i.ibb.co/NyQhDBZ/my-img1.jpg",
+//     title: "Dance Workshop",
+//     students: 1520,
+//   },
+//   {
+//     id: 3,
+//     image: "https://i.ibb.co/37bdwrH/my-img1.png",
+//     title: "Cooking Class",
+//     students: 100,
+//   },
+//   {
+//     id: 4,
+//     image: "https://i.ibb.co/37bdwrH/my-img1.png",
+//     title: "Cooking Class",
+//     students: 7600,
+//   },
+
+// ];
+
+
+
 
 const PopularClasses = () => {
-  const popularClasses = popularClassesData
-    .sort((a, b) => b.students - a.students)
+  const [menu] = useMenu();
+
+  const popularClasses = menu.sort((a, b) => b.students - a.students)
     .slice(0, 6);
+    console.log(popularClasses);
 
   return (
     <div className="bg-gray-100  py-8">
@@ -40,7 +47,7 @@ const PopularClasses = () => {
         <h2 className="text-3xl font-bold text-center my-6 mb-4">Popular Classes</h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
           {popularClasses.map((classItem) => (
-            <div key={classItem.id} className="bg-white p-4 rounded-lg shadow-xl">
+            <div key={classItem._id} className="bg-white p-4 rounded-lg shadow-xl">
               <img src={classItem.image} alt={classItem.title} className="w-full h-80 object-cover mb-4" />
               <h3 className="text-xl font-bold">{classItem.title}</h3>
               <div className="flex items-center justify-between">
