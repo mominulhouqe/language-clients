65b3607fd402d3cf3068597d39d05db9788096df
mominul <ulhouqemomin39@gmail.com>
2023-06-11 17:22:05 +0600

    added instructor class

--- src/pages/Dashboard/Instructors/InstructorsClass.jsx
+++ src/pages/Dashboard/Instructors/InstructorsClass.jsx
@@ -1,43 +1,92 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
+import useMenu from '../../../hooks/useMenu';
+import useAuth from '../../../hooks/useAuth';
+import { AuthContext } from '../../../provider/AuthProvider';
 
 const InstructorsClass = () => {
-  const [classes, setClasses] = useState([]);
+  
+  const { user } = useContext(AuthContext);
+  const [menu] = useMenu();
 
-  const handleAddClass = (newClass) => {
-    setClasses([...classes, newClass]);
-  };
+  const renderUserItems = () => {
+    const filteredItems = menu.filter((item) => item.email === user?.email);
 
-  const handleUpdateClass = (classId, updatedClass) => {
-    const updatedClasses = classes.map((c) =>
-      c.id === classId ? updatedClass : c
-    );
-    setClasses(updatedClasses);
-  };
-
-  const renderClasses = () => {
-    if (classes.length === 0) {
-      return <p>No classes added yet.</p>;
+    if (filteredItems.length === 0) {
+      return <p className="text-gray-600 text-lg">No Class items found.</p>;
     }
 
-    return classes.map((c) => (
-      <div key={c.id}>
-        <h3>{c.className}</h3>
-        <p>Status: {c.status}</p>
-        {c.status === 'denied' && <p>Feedback: {c.feedback}</p>}
-        <p>Total Enrolled Students: {c.enrolledStudents.length}</p>
-        <button onClick={() => handleUpdateClass(c.id, { ...c, status: 'approved' })}>
-          Update
-        </button>
+    return filteredItems.map((item) => (
+      <div
+        key={item.id}
+        className="bg-white w-full mx-6 shadow-md rounded-lg p-6 mb-6 flex flex-col md:flex-row items-center"
+      >
+        <img
+          src={item.image}
+          alt={item.name}
+          className="w-96  rounded-2xl mr-4 mb-4 md:mb-0"
+        />
+        <div className='mx-6' >
+          <h3 className="text-xl font-bold mb-2">{item.instructor}</h3>
+          <p className="text-gray-600 mb-2">Name: {item.name}</p>
+          <p className="text-gray-600">Available Seats: {item.availableseats}</p>
+          <p className="text-gray-600">Price: ${item.price}</p>
+          <p className="text-gray-600">
+            Status: {item.status === 'pending' ? 'Pending' : item.status === 'approved' ? 'Approved' : 'Denied'}
+          </p>
+          <p className="text-gray-600">
+            {/* Total Enrolled Students: {item.enrolledStudents.length} */}
+          </p>
+          {item.status === 'denied' && (
+            <div className="mt-2">
+              <p className="text-gray-600">Feedback: {item.feedback}</p>
+            </div>
+          )}
+          <button className="btn btn-primary hover:bg-blue-600 text-white py-2 px-4 rounded-md mt-4">
+            Update
+          </button>
+        </div>
       </div>
     ));
   };
 
   return (
     <div>
-      <h1>My Classes</h1>
-      {renderClasses()}
+      <h2 className="text-2xl font-bold mb-4">My Classes</h2>
+      {renderUserItems()}
     </div>
   );
 };
 
 export default InstructorsClass;
+
+
+
+
+
+
+// const handleAddClass = (newClass) => {
+  //   setClasses([...classes, newClass]);
+  // };
+
+  // const renderClasses = () => {
+  //   const instructorClasses = classes.filter((c) => c.instructorEmail === user?.email);
+
+  //   if (instructorClasses.length === 0) {
+  //     return <p>No classes added yet.</p>;
+  //   }
+
+  //   return instructorClasses.map((c) => (
+  //     <div key={c.id} className="bg-gray-100 p-4 mb-4">
+  //       <h3 className="text-xl font-bold">{c.name}</h3>
+  //       <p className="mb-2">Status: {c.status}</p>
+  //       {c.status === 'denied' && <p>Feedback: {c.feedback}</p>}
+  //       <p>Total Enrolled Students: {c.enrolledStudents.length}</p>
+  //       <button
+  //         className="bg-blue-500 text-white py-2 px-4 rounded mt-2"
+  //         onClick={() => handleUpdateClass(c.id, { ...c, status: 'approved' })}
+  //       >
+  //         Update
+  //       </button>
+  //     </div>
+  //   ));
+  // };
